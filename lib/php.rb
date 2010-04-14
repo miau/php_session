a0e8be821118f3306edd5e930f38e40565ae74eb
Koji Ando <miau.jp@gmail.com>
2010-04-14 23:28:57 +0900

    Stored PHPSessionParser written by Raphael Bauduin.
    http://groups.google.com/group/comp.lang.ruby/browse_thread/thread/dcfdb288c70b3ca4?pli=1

---
+++ lib/php.rb
@@ -0,0 +1,221 @@
+#!/usr/bin/env ruby
+
+# Copyright Easynet Belgium
+# Licensed under the LGPL
+
+
+require 'cgi'
+require 'cgi/session'
+require 'dbi'
+
+class PHPSessionParser
+
+	attr_reader :hash
+
+	def initialize(str="")
+		@working_str = str
+		@hash = update_hash(str)
+	end
+
+	def update_hash(str)
+		new_hash = {}
+		while more_data?
+			var_name =  extract_var_name
+			type = extract_var_type
+			new_hash[var_name]=send("extract_#{type}") if var_name and type
+		end
+		new_hash
+	end
+
+	def [](key)
+		@hash[key]
+	end
+
+	def []=(key,val)
+		@hash[key]=val
+	end
+
+	def more_data?
+		@working_str and @working_str.length>0
+	end
+	
+	def extract_i
+			value,@working_str = @working_str[@working_str.index(/\d/)..@working_str.length].split(/[;:]/,2)
+			value.to_i
+	end
+
+	def extract_s
+			length, @working_str  = @working_str[1..@working_str.length].split(":",2)
+			value = @working_str[1..length.to_i]
+			@working_str = @working_str.sub(@working_str[0..length.to_i+2],"")
+			value
+	end
+
+	def extract_var_name
+		var_name = nil
+		if index = @working_str.index(/\w/)
+			var_name , @working_str = @working_str[index..@working_str.length].split("|",2)
+			var_name
+		else 
+			@working_str=""
+		end
+		return var_name
+	end
+
+	def extract_var_type
+		index = nil
+		if index=@working_str.index(/[ais]/)
+			type, @working_str =  @working_str[index..@working_str.length].split("",2)
+			type
+		else
+			@working_str=""
+		end
+	end
+
+
+	def extract_a
+			ret={}
+			number_of_elements=@working_str[/\d+/]
+			trash, @working_str = @working_str.split(number_of_elements,2)
+			number_of_elements.to_i.times do |i|
+				key_type = extract_var_type
+				key = send("extract_#{key_type}")
+				value_type = extract_var_type
+                value = send("extract_#{value_type}")
+				ret[key] = value
+			end
+		ret
+	end
+
+	def to_php_session
+		result=""
+		@hash.each_pair do |key,value|
+			result+=value.to_php_session(key)
+		end
+		result
+	end
+	
+
+end
+
+class String 
+	def to_php_session(key=nil)
+		return "#{key}|s:#{length}:\"#{self}\";" if key
+#		puts "s:#{length}:\"#{self}\";"
+		"s:#{length}:\"#{self}\";"
+	end
+	
+end
+class Integer
+	def to_php_session(key=nil)
+		return "#{key}|i:#{to_s};" if key
+		return "i:#{to_s};"
+	end
+end
+
+class Fixnum
+	def to_php_session(key=nil)
+		return "#{key}|i:#{to_s};" if key
+		return "i:#{to_s};"
+	end
+end
+
+class Hash
+	def to_php_session(key = nil)
+#puts "in hash"
+		str = ""
+		str += "#{key}|"   if key
+		str += "a:#{length}:{"
+		each_pair do |k,value|
+			str+="#{k.to_php_session(nil)}#{value.to_php_session}"
+		end
+		str+="}"
+		str
+	end
+end
+
+class Array
+	def to_php_session(key = nil)
+		str = ""
+		str += "#{key}|"   if key
+		str += "a:#{length}:{"
+		each_index do |i|
+			str+="#{i.to_php_session(nil)}#{at(i).to_php_session}"
+		end
+		str+="}"
+		str
+	end
+end
+
+
+
+
+class CGI
+	class Session
+		def []=(key, val)
+		  unless @write_lock
+			@write_lock = true
+		  end
+		  unless @data
+			@data = @dbman.restore
+		  end
+		  @data[key] = val
+		end
+		class PHPDBIStore
+			def initialize(session, options = {} )
+				@session_id = session.session_id
+				@user		= options["user"]
+				@password	= options["password"]
+				@dbi_url	= options["dbi_url"]
+				@dbi_params = options["dbi_params"] || {}
+				@con		= DBI.connect(@dbi_url, @user, @password) 
+				@php_session= PHPSessionParser.new("")
+				restore
+			end
+			def restore
+				#puts "restoring session with id #{@session_id}"
+				query = "select data from php_sessions where session_id=?"
+#				puts query
+				begin 
+					@php_session = PHPSessionParser.new(@con.select_one(query,@session_id)[0])
+				rescue NoMethodError
+#					puts "this is a new session"
+					@php_session = PHPSessionParser.new("")
+				end
+				
+				@php_session.hash
+			end
+
+			def update
+#				puts "in update"
+				return unless @php_session
+				count_query = "select count(*) from php_sessions where  session_id=?"
+				if @con.select_one(count_query, @session_id)[0].to_i<1
+					query = "insert into php_sessions (data,session_id) VALUES (?,?)"
+				else
+					query = "update php_sessions set data=? where session_id=?"
+				end
+#				puts query
+				@con.execute(query,@php_session.to_php_session, @session_id) 
+			end
+
+			def close
+				update
+			end
+		end
+	end
+
+end
+
+if $0 == __FILE__
+  STDIN.reopen("/dev/null")
+  cgi = CGI.new
+  session = CGI::Session.new(cgi, 'session_id' => "9435b4f48a705b3a748693156e39bd34", 'database_manager' => CGI::Session::PHPPostgresqlStore, :user => "dbuser", :password => 'password', :dbi_url => 'dbi:Pg:php_sessions:dbhost')
+  session['key']={'k' => 'v'}
+  puts session['key'].class
+  fail unless Hash === session['key']
+  puts session['key'].inspect
+  session.update
+  fail unless session['key'].inspect == '{"k"=>"v"}'
+end
+
