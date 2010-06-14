daeb123dc3dcf9a5e17d02174a4a4d5d45057c5e
Koji Ando <miau.jp@gmail.com>
2010-06-14 20:45:22 +0900

    Had PhpSession inherite ActiveRecord::SessionStore::Session.
    Methods inherited from ActiveRecord::SessionStore::Session show much
    better performance than those from ActiveRecord::SessionStore::SqlBypass.

--- lib/php_session.rb
+++ lib/php_session.rb
@@ -2,16 +2,14 @@
 require 'php_session_parser'
 require 'active_record'
 
-class PhpSession < ActiveRecord::SessionStore::SqlBypass
+class PhpSession < ActiveRecord::SessionStore::Session
   ##
   # :singleton-method:
   # The last-modified-time field defaults to 'last_active'.
   cattr_accessor :last_active_column
   @@last_active_column = 'last_active'
 
-  @@table_name = 'php_sessions'
-  @@session_id_column = 'session_id'
-  @@data_column = 'data'
+  before_save :set_last_active!
 
   class << self
     def marshal(data)
@@ -23,34 +21,9 @@ class PhpSession < ActiveRecord::SessionStore::SqlBypass
     end
   end
 
-  def save
-    return false if !loaded?
-    marshaled_data = self.class.marshal(data)
-
-    if @new_record
-      @new_record = false
-      begin
-        return @@connection.update <<-end_sql, 'Create session'
-          INSERT INTO #{@@table_name} (
-            #{@@connection.quote_column_name(@@session_id_column)},
-            #{@@connection.quote_column_name(@@data_column)},
-            #{@@connection.quote_column_name(@@last_active_column)}
-          ) VALUES (
-            #{@@connection.quote(session_id)},
-            #{@@connection.quote(marshaled_data)},
-            #{@@connection.quote(Time.now.to_f)}
-          )
-        end_sql
-      rescue => e
-        # Sometimes duplicate key errors caused by race condition occur.
-        # When it occurs, try update statement.
-      end
+  private
+    def set_last_active!
+      return false if !loaded?
+      write_attribute(@@last_active_column, Time.now.to_f)
     end
-    @@connection.update <<-end_sql, 'Update session'
-      UPDATE #{@@table_name}
-      SET #{@@connection.quote_column_name(@@data_column)} = #{@@connection.quote(marshaled_data)},
-          #{@@connection.quote_column_name(@@last_active_column)} = #{@@connection.quote(Time.now.to_f)}
-      WHERE #{@@connection.quote_column_name(@@session_id_column)} = #{@@connection.quote(session_id)}
-    end_sql
-  end
 end
